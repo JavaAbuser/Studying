d3fcadf1af4ab6704aa6be945195f4a028d4a4d7
Max <43775453+JavaAbuser@users.noreply.github.com>
2022-01-28 03:46:32 +0200

    updated OwnReflectionAPI.class

--- ReflectionAPI/ReflectionAPI/OwnReflectionAPI.java
+++ ReflectionAPI/ReflectionAPI/OwnReflectionAPI.java
@@ -2,8 +2,10 @@ package ReflectionAPI.ReflectionAPI;
 
 import ReflectionAPI.Reflection.Human;
 
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
 
 public class OwnReflectionAPI {
     private Human human = null;
@@ -11,8 +13,6 @@ public class OwnReflectionAPI {
     public OwnReflectionAPI(Human human) {
         this.human = human;
     }
-
-    OwnReflectionAPI ownReflectionAPI = new OwnReflectionAPI(human);
 
     public Class<?> getClassClass() throws ClassNotFoundException {
         Class<?> c1 = Human.class;
@@ -23,7 +23,7 @@ public class OwnReflectionAPI {
 
     public void getPublicFieldsInfo() throws ClassNotFoundException, IllegalAccessException {
         System.out.println("\t\tPublic fields: ");
-        Field[] publicFields = ownReflectionAPI.getClassClass().getFields();
+        Field[] publicFields = getClassClass().getFields();
         for(Field publicField : publicFields){
             System.out.println("\tName: " + publicField.getName());
             System.out.println("\tType: " + publicField.getType().getName());
@@ -33,7 +33,7 @@ public class OwnReflectionAPI {
 
     public void getFieldsInfo() throws ClassNotFoundException, IllegalAccessException {
         System.out.println("\t\tDeclared fields: ");
-        Field[] fields = ownReflectionAPI.getClassClass().getDeclaredFields();
+        Field[] fields = getClassClass().getDeclaredFields();
         for(Field field : fields){
             System.out.println("\tName: " + field.getName());
             System.out.println("\tType: " + field.getType().getName());
@@ -44,10 +44,23 @@ public class OwnReflectionAPI {
 
     public void getMethodInfo() throws ClassNotFoundException {
         System.out.println("\t\tDeclared Methods: ");
-        Method[] methods = ownReflectionAPI.getClassClass().getDeclaredMethods();
+        Method[] methods = getClassClass().getDeclaredMethods();
         for(Method method : methods){
             System.out.println("\tName: " + method.getName());
             System.out.println("\tReturn type: " + method.getReturnType());
+        }
+    }
+    public void getConstructorInfo() throws ClassNotFoundException {
+        System.out.println("Constructors: ");
+        Constructor[] constructors = getClassClass().getConstructors();
+        for(Constructor constructor : constructors){
+            Parameter[] parameters = constructor.getParameters();
+            System.out.println("\tConstructor parameters: ");
+            int i = 0;
+            for(Parameter parameter : parameters){
+                i++;
+                System.out.println("\t\t\t " + i + ". " + parameter.getType().getName());
+            }
         }
     }
 }
