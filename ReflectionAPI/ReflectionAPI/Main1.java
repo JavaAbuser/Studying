dca209e57b0d1698dce656c5f789c85ef90ab4f2
Max <43775453+JavaAbuser@users.noreply.github.com>
2022-01-28 03:45:57 +0200

    updated Main1.class

--- ReflectionAPI/ReflectionAPI/Main1.java
+++ ReflectionAPI/ReflectionAPI/Main1.java
@@ -1,5 +1,17 @@
 package ReflectionAPI.ReflectionAPI;
 
-public class Main1 {
+import ReflectionAPI.Reflection.Human;
 
+import java.util.Date;
+
+public class Main1 {
+    public static void main(String[] args) throws ClassNotFoundException, IllegalAccessException {
+        Human human = new Human("Daniel", 23, new Date());
+        OwnReflectionAPI ownReflectionAPI = new OwnReflectionAPI(human);
+
+        ownReflectionAPI.getPublicFieldsInfo();
+        ownReflectionAPI.getFieldsInfo();
+        ownReflectionAPI.getMethodInfo();
+        ownReflectionAPI.getConstructorInfo();
+    }
 }
